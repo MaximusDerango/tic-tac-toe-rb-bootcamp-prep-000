780f6059e7e7207b9705808784b13f8c94d6e6e3
Maxim Dalton <maximcdalton@gmail.com>
2018-06-04 01:53:51 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -23,5 +23,5 @@ def position_taken?(board, index)
 end
 
 def valid_move?(board, index)
-  index.between(0, 8) && !position_taken?(board, index)
+  index.between?(0, 8) && !position_taken?(board, index)
 end
