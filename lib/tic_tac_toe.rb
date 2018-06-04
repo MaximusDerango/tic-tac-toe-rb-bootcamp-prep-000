80d1e5f04dcc21365839d1e1075cbad74d119968
Maxim Dalton <maximcdalton@gmail.com>
2018-06-04 01:44:19 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,3 +3,9 @@ WIN_COMBINATIONS = [
   [0, 3, 6], [1, 4, 7], [2, 5, 8]
   [0, 4, 8], [2, 4, 6]
 ]
+
+def display_board(board)
+  rows = (0..8).step(3).collect{|i| (i..i+2).collect{|j|" #{board[i]}} "}}.join("|")
+  div = "\n" +  "-"*11 + "\n"
+  row.join(div)
+end
